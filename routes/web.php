d41402f63f05821acabcf5bceba60743b81df53c
AmilaKMAC <achathuranga452@gmail.com>
2026-05-20 16:41:34 +0530

    created web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AnalysisController;
+use App\Http\Controllers\ReportController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::post('/analyze', [AnalysisController::class, 'analyze'])->name('analyze');
+
+Route::get('/report/{report}', [ReportController::class, 'show'])->name('report.show');
