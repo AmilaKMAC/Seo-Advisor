5598d87a13d36b2975ef5c9a58acb0c5cdb78002
AmilaKMAC <achathuranga452@gmail.com>
2026-05-20 16:49:23 +0530

    app.blade.php updated

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>SEO Advisor</title>
+    @vite(['resources/css/app.css'])
+</head>
+
+<body class="bg-gray-100">
+
+<div class="flex min-h-screen">
+
+    <!-- Sidebar -->
+    @include('components.sidebar')
+
+    <!-- Main -->
+    <div class="flex-1">
+
+        @include('components.header')
+
+        <main class="p-6">
+            @yield('content')
+        </main>
+
+    </div>
+
+</div>
+
+</body>
+</html>
+``
